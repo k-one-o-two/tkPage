063b91d445bd1ddf8f4a1f9a9680d7d49c0258f8
Kirill Sergeev <k.one.o.two@gmail.com>
2025-02-15 13:50:59 +0200

    no crt on mob

--- .next/static/webpack/webpack.99d64383bddec369.hot-update.js
+++ .next/static/webpack/webpack.99d64383bddec369.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "c3fb638cdf3d7cae"; }
+/******/ 	__webpack_require__.h = function() { return "7ac6fa92400e24e7"; }
 /******/ }();
 /******/ 
 /******/ }
