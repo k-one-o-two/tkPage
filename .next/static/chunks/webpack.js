24e76da9d96ddd134dce131c76f6262707639169
Kirill Sergeev <k.one.o.two@gmail.com>
2025-02-23 14:14:10 +0200

    ascii

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "eadf13ff469f1898"; }
+/******/ 		__webpack_require__.h = function() { return "377a75bb0d06cf81"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/card.js
+++ components/card.js
@@ -2,16 +2,13 @@
 export function Card({ title, children, link, art }) {
 
   return (
-    <a href={link}>
-      <div
-        className="card card-l2"
-        style={{display: 'flex', flexWrap: 'wrap'}}
-      >
-        <div><pre><code>{art}</code></pre></div>
+    <div className="card card-l2" style={{display: 'flex', flexWrap: 'wrap', gap: 10}}>
+      <div><pre>{art}</pre></div>
+      <a href={link}>
         <div><h2 className="title">{title}</h2>
         <div className="content">{children}</div></div>
-      </div>
-    </a>
+      </a>
+    </div>
   );
 }
 
