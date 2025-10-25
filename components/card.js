502c8fd68c37685102ea161866313bfe9b0f6310
Kirill Sergeev <k.one.o.two@gmail.com>
2025-10-25 13:35:31 +0300

    fonts

--- components/card.js
+++ components/card.js
@@ -4,24 +4,16 @@ export function Card({ title, children, link, art }) {
   const router = useRouter();
 
   return (
-    <div
-      className="card card-l2"
-      style={{
-        display: "flex",
-        flexWrap: "wrap",
-        gap: 10,
-        alignItems: "center",
-      }}
-    >
+    <div onClick={() => router.push(link)} className="card card-l2 card-link">
       <div>
         <pre>{art}</pre>
       </div>
-      <span onClick={() => router.push(link)} className="link">
+      <div>
         <div>
           <h2 className="title">{title}</h2>
           <div className="content">{children}</div>
         </div>
-      </span>
+      </div>
     </div>
   );
 }
